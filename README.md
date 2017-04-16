352b70a6cafeea7064a4c1bd0b1d34612f499f51
Milo4321 <mocmil@wp.pl>
2017-04-16 22:33:22 +0200

    Now application has its servlet/jsp test page

--- README.md
+++ README.md
@@ -1,18 +1,20 @@
 DMMsoftware  Finance-app
 
 
-Build & run
+#### Build & run
 
 Run Maven command from the project root:
 
-    mvn wildfly:run
-     
+    $ mvn wildfly:run  
 
-To quit wildfly: [CTRL]+[C] 
-    
+To quit wildfly: CTRL+C
 
-Configuration of application web container Wildfly plugin
+To clean maven wildfly last build:
+  
+    $ mvn clean
 
-default port:8080
+test pages:
 
-test page: http://localhost:8080/financial-app/index.html
+plain html: http://localhost:8080/financial-app/index.html
+
+servlet/jsp: http://localhost:8080/financial-app/test
--- src/main/java/com/dmmsoft/TestServlet.java
+++ src/main/java/com/dmmsoft/TestServlet.java
@@ -1,4 +1,4 @@
-/**
+package com.dmmsoft; /**
  * Created by milo on 15.04.17.
  */
 
@@ -11,14 +11,13 @@ import com.dmmsoft.app.FileIO.Path.FilePath;
 import com.dmmsoft.app.POJO.Investment;
 import com.dmmsoft.app.POJO.MainContainer;
 import com.dmmsoft.app.POJO.Quotation;
-import com.dmmsoft.app.DataLoader.*;
-import com.sun.net.httpserver.HttpServer;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.core.MediaType;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -27,9 +26,59 @@ import java.util.stream.Collectors;
 
 
 
+@WebServlet(urlPatterns = "/test")
+public class TestServlet extends HttpServlet {
 
-public class testServlet extends HttpServlet {
+    private static final String TEST_INV_NAME= "AGI001";
 
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+       AppConfigurationProvider appCon = new AppConfigurationProvider().getConfiguration();
+       MainContainerLoader mainContainerLoader = new MainContainerLoader(appCon);
+       mainContainerLoader.loadFunds();
+       mainContainerLoader.loadCurrencies();
+
+       MainContainer mdc = mainContainerLoader.getMainContainer();
+       List<Investment> investments = mdc.getInvestments();
+
+       ItemStatsResult s = new ItemStats().getResult(investments, TEST_INV_NAME);
+       // System.out.println(s.toString());
+
+        resp.setContentType(MediaType.TEXT_HTML);
+       // resp.getWriter().print(s.toString());
+
+        req.setAttribute("investmentName", s.getName());
+        req.setAttribute("firstQuotationDate",s.getFirstQuotation().getDate());
+        req.setAttribute("firstQuotationValue",s.getFirstQuotation().getClose());
+
+        req.setAttribute("lastQuotationDate", s.getLastQuotation().getDate());
+        req.setAttribute("lastQuotationValue", s.getLastQuotation().getClose());
+
+        req.setAttribute("maxDeltaPlusDate", s.getMaxDeltaPlus().getDate());
+        req.setAttribute("maxDeltaPlusValue", s.getMaxDeltaPlus().getDeltaClose());
+
+        req.setAttribute("maxDeltaMinusDate",s.getMaxDeltaMinus().getDate());
+        req.setAttribute("maxDeltaMinusValue",s.getMaxDeltaMinus().getDeltaClose());
+
+        req.setAttribute("maxValueQuotationDate", s.getMaxValueQuotation().getDate());
+        req.setAttribute("maxValueQuotationValue", s.getMaxValueQuotation().getClose());
+        req.setAttribute("maxValueQuotationDelta", s.getMaxValueQuotation().getDeltaClose());
+
+        req.setAttribute("minValueQuotationDate", s.getMinValueQuotation().getDate());
+        req.setAttribute("minValueQuotationValue", s.getMinValueQuotation().getClose());
+        req.setAttribute("minValueQuotationDelta", s.getMinValueQuotation().getDeltaClose());
+
+      //  req.setAttribute("actualValue", s.getActualValue().getClose());
+
+
+        req.getRequestDispatcher("testpage.jsp").forward(req, resp);
+
+
+    }
+
+    /*
 
     private void doTest(){
 
@@ -79,7 +128,9 @@ public class testServlet extends HttpServlet {
     ItemStatsResult s = new ItemStats().getResult(investments, "AIP001");
         System.out.println(s.toString());
 
-
 }
+
+*/
+
 
 }
