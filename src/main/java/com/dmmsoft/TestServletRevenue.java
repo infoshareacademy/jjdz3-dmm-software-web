20efb5ef5088b18597ea0de5a5b67dcd97db9489
Milo4321 <mocmil@wp.pl>
2017-05-16 00:50:53 +0200

    Now AnalysisRevenue page supports system analysis messages.

--- src/main/java/com/dmmsoft/TestServletRevenue.java
+++ src/main/java/com/dmmsoft/TestServletRevenue.java
@@ -30,66 +30,71 @@ import java.util.List;
  */
 
 @WebServlet(urlPatterns = "/revenue")
-public class TestServletRevenue extends HttpServlet{
+public class TestServletRevenue extends HttpServlet {
     @Inject
     IDataContainerService container;
 
-   // private static final String TEST_INV_NAME= "AGI001";
+    private static final String CRITERIA_MODERATION_MESSAGE = "Note! Your input data does not correspond to current investment history of quotations. \n" +
+            "    For analysis system used nearest possible quoutations acording to dates from submitted form.\n" +
+            "    User criteria moderated by systen are listed in User input moderation report.";
+
+    private static final String NO_DATA_FOR_CRITERIA_MESSAGE = "Error! No data for current criteria!";
 
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws  ServletException, IOException  {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-            req.getRequestDispatcher("testpageRevenue.jsp").forward(req, resp);
-
+        req.getRequestDispatcher("testpageRevenue.jsp").forward(req, resp);
 
 
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws  ServletException, IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
         try {
 
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
-       // LocalDate BUY_DATE = LocalDate.parse("20090910", formatter);
-       // LocalDate SELL_DATE = LocalDate.parse("20170402", formatter);
-       // String InvestmentName = "CHF";
+            // LocalDate BUY_DATE = LocalDate.parse("20090910", formatter);
+            // LocalDate SELL_DATE = LocalDate.parse("20170402", formatter);
+            // String InvestmentName = "CHF";
 
-        String InvestmentName = req.getParameter("investmenName");
-        String sCapital = req.getParameter("capital");
-        String SBUY_DATE = req.getParameter("buyDate");
-        String SSELL_DATE = req.getParameter("sellDate");
+            String InvestmentName = req.getParameter("investmenName");
+            String sCapital = req.getParameter("capital");
+            String SBUY_DATE = req.getParameter("buyDate");
+            String SSELL_DATE = req.getParameter("sellDate");
 
-            if (SBUY_DATE==null || SBUY_DATE.toString().isEmpty())
+            if (SBUY_DATE == null || SBUY_DATE.toString().isEmpty())
                 SBUY_DATE = "2009-09-10";
 
-            if(SSELL_DATE==null || SSELL_DATE.toString().isEmpty())
+            if (SSELL_DATE == null || SSELL_DATE.toString().isEmpty())
                 SSELL_DATE = "2017-04-02";
 
-            if(sCapital==null|| sCapital.toString().isEmpty())
+            if (sCapital == null || sCapital.toString().isEmpty())
                 sCapital = "120000";
 
-            if(InvestmentName==null|| InvestmentName.isEmpty())
-                InvestmentName="CHF";
+            if (InvestmentName == null || InvestmentName.isEmpty())
+                InvestmentName = "CHF";
 
 
-        Double capital = Double.valueOf(sCapital);
-        LocalDate BUY_DATE = LocalDate.parse(SBUY_DATE , formatter);
-        LocalDate SELL_DATE = LocalDate.parse(SSELL_DATE, formatter);
-
+            Double capital = Double.valueOf(sCapital);
+            LocalDate BUY_DATE = LocalDate.parse(SBUY_DATE, formatter);
+            LocalDate SELL_DATE = LocalDate.parse(SSELL_DATE, formatter);
 
 
             InvestmentRevenueCriteria input = new InvestmentRevenueCriteria(capital, BUY_DATE, SELL_DATE, InvestmentName, Boolean.valueOf(false));
             InvestmentRevenueResult ir = (new InvestmentRevenue(container.getMainContainer(), input)).getResult();
 
+
             resp.setContentType(MediaType.TEXT_HTML);
 
-            req.setAttribute("i_investmentName", input.getInvestmentName());
-            req.setAttribute("i_investedCapital", input.getInvestedCapital());
-            req.setAttribute("i_buyDate", input.getBuyDate());
-            req.setAttribute("i_sellDate", input.getSellDate());
+            req.setAttribute("investmentRevenueCriteria", input);
+            req.setAttribute("investmentRevenueResult", ir);
+
+            if (ir.getFinallyEvaluatedInput().getModifiedBySuggester() == true) {
+                req.setAttribute("message", CRITERIA_MODERATION_MESSAGE);
+            }
 
             InvestmentRevenueCriteria finallyEvaluatedInput = (InvestmentRevenueCriteria) ir.getFinallyEvaluatedInput();
             req.setAttribute("f_buyDate", finallyEvaluatedInput.getBuyDate());
@@ -100,10 +105,10 @@ public class TestServletRevenue extends HttpServlet{
             req.setAttribute("r_capitalRevenueDelta", ir.getCapitalRevenueDeltaPrecentValue());
 
             req.getRequestDispatcher("testpageRevenue.jsp").forward(req, resp);
-        }
-        catch (NoDataForCriteria ex){
-
-             System.out.print(ex.getMessage());
+        } catch (NoDataForCriteria ex) {
+            req.setAttribute("message", NO_DATA_FOR_CRITERIA_MESSAGE);
+            req.getRequestDispatcher("testpageRevenue.jsp").forward(req, resp);
+            System.out.print(ex.getMessage());
 
         }
 
