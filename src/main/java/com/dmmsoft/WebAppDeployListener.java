daabf179a454e9e91a84ea41d3b9c24e1ec5549d
Milo4321 <mocmil@wp.pl>
2017-06-19 19:05:57 +0000

    Now application has default timezone set to UTC

--- src/main/java/com/dmmsoft/WebAppDeployListener.java
+++ src/main/java/com/dmmsoft/WebAppDeployListener.java
@@ -1,6 +1,7 @@
 package com.dmmsoft;
 
 import com.dmmsoft.container.IModelContainerService;
+import org.joda.time.tz.UTCProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -8,6 +9,7 @@ import javax.inject.Inject;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
+import java.util.TimeZone;
 
 /**
  * Created by milo on 02.06.17.
@@ -17,26 +19,42 @@ import javax.servlet.annotation.WebListener;
 public final class WebAppDeployListener implements ServletContextListener {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(WebAppDeployListener.class);
+    private static final String DEFAULT_TIMEZONE = "UTC";
+
     @Inject
     IModelContainerService container;
 
 
     @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
-        try {
-            LOGGER.info("Application Deployed. Data model CSV files loading initialized...");
-            container.getMainContainer();
-            LOGGER.info("Application Deployed. Main Container initialized: CSV files successfully loaded.");
-            LOGGER.info("Currencies items:{} Funds items:{}",
-                    container.getMainContainer().getCurrenciesCount(),
-                    container.getMainContainer().getFundsCount());
-        } catch (RuntimeException ex) {
-            LOGGER.error("FATAL ERROR: Failed to load data model CSV files! {}", ex.getMessage());
-        }
+
+        setApplicationDefaultTimeZone(DEFAULT_TIMEZONE);
+        loadCSVFiles();
     }
 
     @Override
     public void contextDestroyed(ServletContextEvent servletContextEvent) {
         LOGGER.info("Application Servlet Context Destroyed.");
     }
+
+    private void loadCSVFiles() {
+        try {
+            LOGGER.info("Application Deployed. Data model CSV files loading initialized...");
+
+            container.getMainContainer();
+            LOGGER.info("Currencies items:{} Funds items:{}",
+                    container.getMainContainer().getCurrenciesCount(),
+                    container.getMainContainer().getFundsCount());
+            LOGGER.info("Application Deployed. Main Container initialized: CSV files successfully loaded.");
+        } catch (RuntimeException ex) {
+            LOGGER.error("FATAL ERROR: Failed to load data model CSV files! {}", ex.getMessage());
+        }
+
+    }
+
+    private void setApplicationDefaultTimeZone(String timeZone) {
+        TimeZone.setDefault(TimeZone.getTimeZone(timeZone));
+        LOGGER.info("Default application timezone set to: {}", TimeZone.getDefault());
+    }
+
 }
