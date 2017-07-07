fcb92cf518fc23f8c298e634f8c25685e946ffee
Milo4321 <mocmil@wp.pl>
2017-07-07 13:50:09 +0200

    Now Jar dependency updated with new feature: remote csv load.

--- src/main/java/com/dmmsoft/container/ModelContainer.java
+++ src/main/java/com/dmmsoft/container/ModelContainer.java
@@ -1,9 +1,9 @@
 package com.dmmsoft.container;
 
 import com.dmmsoft.app.appconfiguration.AppConfigurationProvider;
-import com.dmmsoft.app.dataloader.MainContainerLoader;
-import com.dmmsoft.app.model.Investment;
+import com.dmmsoft.app.model.loader.MainContainerLoader;
 import com.dmmsoft.app.model.MainContainer;
+import com.dmmsoft.app.model.Investment;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.Singleton;
@@ -13,12 +13,13 @@ import java.util.List;
  * Created by milo on 06.05.17.
  */
 
+
+
 @Singleton
 public class ModelContainer implements IModelContainerService {
 
     private List<Investment> investments;
-    private com.dmmsoft.app.model.MainContainer mainContainer;
-
+    private MainContainer mainContainer;
 
     @PostConstruct
     private void onPostConstruct() {
@@ -27,10 +28,10 @@ public class ModelContainer implements IModelContainerService {
         mainContainerLoader.loadFunds();
         mainContainerLoader.loadCurrencies();
 
-        // jar dependency
-        com.dmmsoft.app.model.MainContainer mainContainer = mainContainerLoader.getMainContainer();
-        this.investments = mainContainer.getInvestments();
+        MainContainer mainContainer = mainContainerLoader.getMainContainer();
         this.mainContainer = mainContainer;
+        this.investments = mainContainer.getInvestments();
+
     }
 
     public void reload(){
